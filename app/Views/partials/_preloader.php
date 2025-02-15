edbea50f971f8b0447248c6ed3c92c0237d63f70
Andi Eka Nugraha <an.ekanugraha@gmail.com>
2025-02-15 11:19:12 +0700

    fix map home page

--- app/Views/partials/_preloader.php
+++ app/Views/partials/_preloader.php
@@ -6,14 +6,10 @@
             <div class="animation-preloader">
                 <div class="spinner"></div>
                 <div class="txt-loading">
-                    <span data-text-preloader="l" class="letters-loading"> l </span>
-                    <span data-text-preloader="o" class="letters-loading"> o </span>
-                    <span data-text-preloader="g" class="letters-loading"> g </span>
-                    <span data-text-preloader="i" class="letters-loading"> i </span>
-                    <span data-text-preloader="s" class="letters-loading"> s </span>
-                    <span data-text-preloader="t" class="letters-loading"> t </span>
-                    <span data-text-preloader="i" class="letters-loading"> i </span>
-                    <span data-text-preloader="q" class="letters-loading"> q </span>
+                    <span data-text-preloader="T" class="letters-loading"> T </span>
+                    <span data-text-preloader="O" class="letters-loading"> O </span>
+                    <span data-text-preloader="R" class="letters-loading"> R </span>
+                    <span data-text-preloader="O" class="letters-loading"> O </span>
                 </div>
             </div>
         </div>
--- app/Views/partials/home/banner.php
+++ app/Views/partials/home/banner.php
@@ -11,13 +11,13 @@
                         <div class="round-box">
                             <div class="bdr"></div>
                         </div>
-                        <div class="content-box">
+                        <div class="content-box d-xl-block d-none">
                             <div class="img-box">
                                 <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
                             </div>
                             <div class="text-box">
-                                <h4>London</h4>
-                                <p>Logistic service <br> provider </p>
+                                <h4>Aceh</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
                             </div>
                         </div>
                     </div>
@@ -26,13 +26,13 @@
                         <div class="round-box">
                             <div class="bdr"></div>
                         </div>
-                        <div class="content-box">
+                        <div class="content-box d-xl-block d-none">
                             <div class="img-box">
                                 <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
                             </div>
                             <div class="text-box">
-                                <h4>Alexander City</h4>
-                                <p>Logistic service <br> provider </p>
+                                <h4>Medan</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
                             </div>
                         </div>
                     </div>
@@ -41,13 +41,13 @@
                         <div class="round-box">
                             <div class="bdr"></div>
                         </div>
-                        <div class="content-box">
+                        <div class="content-box d-xl-block d-none">
                             <div class="img-box">
                                 <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
                             </div>
                             <div class="text-box">
-                                <h4>Birmingham</h4>
-                                <p>Logistic service <br> provider </p>
+                                <h4>Padang</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
                             </div>
                         </div>
                     </div>
@@ -56,13 +56,13 @@
                         <div class="round-box">
                             <div class="bdr"></div>
                         </div>
-                        <div class="content-box">
+                        <div class="content-box d-xl-block d-none">
                             <div class="img-box">
                                 <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
                             </div>
                             <div class="text-box">
-                                <h4>Guntersville</h4>
-                                <p>Logistic service <br> provider </p>
+                                <h4>Palembang</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
                             </div>
                         </div>
                     </div>
@@ -71,13 +71,13 @@
                         <div class="round-box">
                             <div class="bdr"></div>
                         </div>
-                        <div class="content-box">
+                        <div class="content-box d-xl-block d-none">
                             <div class="img-box">
                                 <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
                             </div>
                             <div class="text-box">
-                                <h4>Montgomery</h4>
-                                <p>Logistic service <br> provider </p>
+                                <h4>Lampung</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
                             </div>
                         </div>
                     </div>
@@ -87,13 +87,13 @@
                             <div class="bdr"></div>
                         </div>
 
-                        <div class="content-box">
+                        <div class="content-box d-xl-block d-none">
                             <div class="img-box">
                                 <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
                             </div>
                             <div class="text-box">
-                                <h4>California</h4>
-                                <p>Logistic service <br> provider </p>
+                                <h4>Jakarta</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
                             </div>
                         </div>
                     </div>
@@ -102,28 +102,251 @@
                         <div class="round-box">
                             <div class="bdr"></div>
                         </div>
-                        <div class="content-box">
+                        <div class="content-box d-xl-block d-none">
                             <div class="img-box">
                                 <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
                             </div>
                             <div class="text-box">
-                                <h4>Colorado</h4>
-                                <p>Logistic service <br> provider </p>
+                                <h4>Bandung</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
                             </div>
                         </div>
                     </div>
-
                     <div class="banner-one__location-single style8">
                         <div class="round-box">
                             <div class="bdr"></div>
                         </div>
-                        <div class="content-box">
+                        <div class="content-box d-xl-block d-none">
                             <div class="img-box">
                                 <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
                             </div>
                             <div class="text-box">
-                                <h4>Berlin</h4>
-                                <p>Logistic service <br> provider </p>
+                                <h4>Semarang</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style9">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Malang</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style10">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Surabaya</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style11">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Bali</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style12">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Lombok</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style13">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Kupang</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style14">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Pontianak</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style15">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Banjarmasin</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style16">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Balikpapan</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style17">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Palu</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style18">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Makasar</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style19">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Manado</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style20">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Maluku Utara</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style21">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Maluku</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style22">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Sorong</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style23">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Manokwari</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="banner-one__location-single style24">
+                        <div class="round-box">
+                            <div class="bdr"></div>
+                        </div>
+                        <div class="content-box d-xl-block d-none">
+                            <div class="img-box">
+                                <img src="/assets/general/images/banner/banner-v1-flag1.png" alt="">
+                            </div>
+                            <div class="text-box">
+                                <h4>Jayapura</h4>
+                                <p>Antar & Kirim<br>Logistik</p>
                             </div>
                         </div>
                     </div>
